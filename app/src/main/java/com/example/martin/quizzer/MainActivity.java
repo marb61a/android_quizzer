eff3ed5b9ef010c9287b76fe9a6ac0ff3ef4598b
marb61a <marb61a@gmail.com>
2018-01-23 13:28:53 +0000

    updated dimens files, colors & styles and added initial values to main activity

--- app/src/main/java/com/example/martin/quizzer/MainActivity.java
+++ app/src/main/java/com/example/martin/quizzer/MainActivity.java
@@ -2,8 +2,23 @@ package com.example.martin.quizzer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // Member variables that are accessible in all the methods of the main activity
+    Button mTrueButton;
+    Button mFalseButton;
+    TextView mScoreTextView;
+    TextView mQuestionTextView;
+    ProgressBar mProgressBar;
+    int mIndex;
+    int mScore;
+    int mQuestion;
+    Toast mToastMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
