10eeba692625bd3fd74dda12fcf1f3e570007f60
marb61a <marb61a@gmail.com>
2018-01-25 13:21:09 +0000

    Added onclicklisteners to the true false buttons

--- app/src/main/java/com/example/martin/quizzer/MainActivity.java
+++ app/src/main/java/com/example/martin/quizzer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.martin.quizzer;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -19,10 +21,47 @@ public class MainActivity extends AppCompatActivity {
     int mQuestion;
     Toast mToastMessage;
 
+    // Create a question bank using the TrueFalse class for each item in the array
+    @NonNull
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Prior to API 26 casting was needed for using buttons with views
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mScoreTextView = findViewById(R.id.score);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+                updateQuestion();
+            }
+        });
+    }
+
+    public void updateQuestion(){
+
+    }
+
+    public void checkAnswer(boolean userSelection){
+
     }
 }
