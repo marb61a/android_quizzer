297902b4547da0e5a1bdd5d5da0bfdb3760806ac
marb61a <marb61a@gmail.com>
2018-01-30 16:25:21 +0000

    Added question array

--- app/src/main/java/com/example/martin/quizzer/MainActivity.java
+++ app/src/main/java/com/example/martin/quizzer/MainActivity.java
@@ -24,9 +24,24 @@ public class MainActivity extends AppCompatActivity {
     // Create a question bank using the TrueFalse class for each item in the array
     @NonNull
     private TrueFalse[] mQuestionBank = new TrueFalse[]{
-
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13,true)
     };
 
+    // Declaring constants here. Rather than a fixed number, choosing to make it a function
+    // of the length of the question bank (the number of questions)
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0 / mQuestionBank.length);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
         mQuestionTextView = findViewById(R.id.question_text_view);
         mScoreTextView = findViewById(R.id.score);
         mProgressBar = findViewById(R.id.progress_bar);
+
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkAnswer(boolean userSelection){
+        boolean correctAnswer = mQuestionBank[mIndex].isAnswer();
 
     }
 }
